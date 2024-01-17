96f4537a707632c314cf69474168f70a930fe3c7
jonakj <jonakj@stud.ntnu.no>
2024-01-17 19:49:56 +0100

    Main file argument handling for running different features

--- parser/main.go
+++ parser/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "IG-Parser-Stats/statistics"
+import (
+	"IG-Parser-Stats/statistics"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	statistics.RunStatistics()
+	// Take parameters when running the application
+	inputFile := flag.String("input", "input.json", "Input flag for input file")
+	outputFile := flag.String("output", "output.json", "Output flag for output file")
+	mode := flag.Int("mode", 0, "Mode flag for program mode, 0 for the autoRunner, 1 for the statistics runner")
+
+	flag.Parse()
+
+	fmt.Println(*inputFile, *outputFile, *mode)
+
+	switch *mode {
+	case 0:
+		statistics.AutoRunner()
+	case 1:
+		statistics.RunStatistics()
+	}
+
+	// If none found default to the autorunner with the default file path
+
+	//statistics.RunStatistics()
 
 	//statistics.AutoRunner()
 }
