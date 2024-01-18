1195bbbe3f4571b1cb35332e821a9d9032416efe
jonakj <jonakj@stud.ntnu.no>
2024-01-18 11:31:59 +0100

    Added argument for running through all statements in a file.

--- stanza/runnerAdvanced.py
+++ stanza/runnerAdvanced.py
@@ -14,26 +14,47 @@ number = int(args[1])
 filename = "input.json"
 
 i = number
-#i = 0
-#while i < 18: 
+
 # The testData.json is a json file containing an array of objects with a name, baseText and processedText.
 # The important fields are the baseText which is the input statement and the processedText which is the 
 # annotated statement.
 with open(filename, "r") as input:
     jsonData = json.load(input)
-    print(jsonData[i])
 
-#print(jsonData[i]['baseText'])
+# If argument is -1 then go through all the items
+if i == -1:
+    i = 0
 
-output = Matcher(jsonData[i]['baseText'])
-
-print(jsonData[i]['baseText'] + "\n" + jsonData[i]['processedText'] + "\n" + output)
-
-jsonData[i]["stanza"] = output
-
-# Write the automatically parsed statement to the file
-with open(filename, "w") as outputFile:
-    json.dump(jsonData, outputFile, indent=2)
+    print("Running with ", len(jsonData), " items.")
+    while i < len(jsonData): 
         
-#    i += 1
+
+        
+        output = Matcher(jsonData[i]['baseText'])
+
+        print(jsonData[i]['baseText'] + "\n" + jsonData[i]['processedText'] + "\n" + output)
+
+        jsonData[i]["stanza"] = output
+
+        i += 1
+
+    # Write the automatically parsed statement to the file
+    with open(filename, "w") as outputFile:
+        json.dump(jsonData, outputFile, indent=2)
+
+# Else only go through the selected item
+else:
+    #print(jsonData[i]['baseText'])
+
+    output = Matcher(jsonData[i]['baseText'])
+
+    print(jsonData[i]['baseText'] + "\n" + jsonData[i]['processedText'] + "\n" + output)
+
+    jsonData[i]["stanza"] = output
+
+    # Write the automatically parsed statement to the file
+    with open(filename, "w") as outputFile:
+        json.dump(jsonData, outputFile, indent=2)
+            
+    #    i += 1
 
