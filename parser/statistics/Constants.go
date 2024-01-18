c2154ed009efdf905b6b3e9b021c43bbbbbd103c
jonakj <jonakj@stud.ntnu.no>
2024-01-18 09:24:00 +0100

    Moved data type declarations to constants file

--- parser/statistics/Constants.go
+++ parser/statistics/Constants.go
@@ -61,3 +61,49 @@ type inputStructure []struct {
 	StanzaParsed        Statistics `json:"stanzaParsed"`
 	//SpacyParsed         Statistics `json:"spacyParsed"`
 }
+
+// Struct for the ouput file with the components, and a count of each
+type StatisticsAuto struct {
+	//
+	Attributes     []JSONComponent
+	AttributeCount int
+	//
+	DirectObjects     []JSONComponent
+	DirectObjectCount int
+	//
+	IndirectObjects     []JSONComponent
+	IndirectObjectCount int
+	//
+	Aims     []JSONComponent
+	AimCount int
+	//
+	Deontics     []JSONComponent
+	DeonticCount int
+	//
+	OrElses     []JSONComponent
+	OrElseCount int
+	//
+	ActivationConditions     []JSONComponent
+	ActivationConditionCount int
+	//
+	ExecutionConstraints     []JSONComponent
+	ExecutionConstraintCount int
+}
+
+// Struct for each component with text content, a bool for nesting and semantic annotations
+type JSONComponentAuto struct {
+	Content            string
+	Nested             bool
+	SemanticAnnotation string
+}
+
+type inputStructureAuto []struct {
+	Name          string `json:"name"`
+	BaseText      string `json:"baseText"`
+	ProcessedText string `json:"processedText"`
+	Stanza        string `json:"stanzaAdvanced"`
+	//Spacy               string     `json:"stanzaAdvanced"`
+	ProcessedTextParsed StatisticsAuto `json:"processedTextParsed"`
+	StanzaParsed        StatisticsAuto `json:"stanzaParsed"`
+	//SpacyParsed         Statistics `json:"spacyParsed"`
+}
--- parser/statistics/autoRunner.go
+++ parser/statistics/autoRunner.go
@@ -10,52 +10,6 @@ import (
 	"IG-Parser/core/tree"
 )
 
-// Struct for the ouput file with the components, and a count of each
-type StatisticsAuto struct {
-	//
-	Attributes     []JSONComponent
-	AttributeCount int
-	//
-	DirectObjects     []JSONComponent
-	DirectObjectCount int
-	//
-	IndirectObjects     []JSONComponent
-	IndirectObjectCount int
-	//
-	Aims     []JSONComponent
-	AimCount int
-	//
-	Deontics     []JSONComponent
-	DeonticCount int
-	//
-	OrElses     []JSONComponent
-	OrElseCount int
-	//
-	ActivationConditions     []JSONComponent
-	ActivationConditionCount int
-	//
-	ExecutionConstraints     []JSONComponent
-	ExecutionConstraintCount int
-}
-
-// Struct for each component with text content, a bool for nesting and semantic annotations
-type JSONComponentAuto struct {
-	Content            string
-	Nested             bool
-	SemanticAnnotation string
-}
-
-type inputStructureAuto []struct {
-	Name          string `json:"name"`
-	BaseText      string `json:"baseText"`
-	ProcessedText string `json:"processedText"`
-	Stanza        string `json:"stanzaAdvanced"`
-	//Spacy               string     `json:"stanzaAdvanced"`
-	ProcessedTextParsed StatisticsAuto `json:"processedTextParsed"`
-	StanzaParsed        StatisticsAuto `json:"stanzaParsed"`
-	//SpacyParsed         Statistics `json:"spacyParsed"`
-}
-
 func AutoRunner(file string, outFile string) {
 	// Read the input file
 	//file := "./input.json"
