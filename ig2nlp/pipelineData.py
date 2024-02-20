fab216584f9ded81de0542a8e0e4d7c5ff3ed75e
jonakj <jonakj@stud.ntnu.no>
2024-02-20 13:30:00 +0100

    Added two more ner models to the comparison in pipelineData

--- ig2nlp/pipelineData.py
+++ ig2nlp/pipelineData.py
@@ -21,6 +21,19 @@ nlp2 = stanza.Pipeline('en', use_gpu=env['useGPU'],
     download_method=env['downloadMethod'],
     logging_level=env['logLevel'])
 
+nlp3 = stanza.Pipeline('en', use_gpu=env['useGPU'], 
+    processors='tokenize,pos,lemma,ner,mwt', 
+    package={"ner": ["ontonotes-ww-multi_nocharlm"]},
+    download_method=env['downloadMethod'],
+    logging_level=env['logLevel'])
+
+
+nlp4 = stanza.Pipeline('en', use_gpu=env['useGPU'], 
+    processors='tokenize,pos,lemma,ner,mwt', 
+    package={"ner": ["ontonotes-ww-multi_charlm"]},
+    download_method=env['downloadMethod'],
+    logging_level=env['logLevel'])
+
 # Delete the environment variables dictionary
 del env
 
@@ -41,11 +54,15 @@ with open(filename, "r") as input:
 
         docs = nlp.bulk_process(textDocs)
         docs2 = nlp2.bulk_process(textDocs)
+        docs3 = nlp3.bulk_process(textDocs)
+        docs4 = nlp4.bulk_process(textDocs)
 
         i = 0
         while i < len(docs):
             doc = docs[i]
             doc2 = docs2[i]
+            doc3 = docs3[i]
+            doc4 = docs4[i]
             
             print(doc.text)
             outputFile.write("Name: "+ str(i)+"-"+jsonData[i]['name']+ "\n")
@@ -56,6 +73,8 @@ with open(filename, "r") as input:
             while j < len(doc.sentences):
                 sentence = doc.sentences[j]
                 sentence2 = doc2.sentences[j]
+                sentence3 = doc3.sentences[j]
+                sentence4 = doc4.sentences[j]
                 sentence.words = compoundWordsMiddleware(sentence.words)
                 sentence2.words = compoundWordsMiddleware(sentence2.words)
                 # Generating the data structure for displacy visualization
@@ -71,7 +90,7 @@ with open(filename, "r") as input:
                 outputFile.write("\nWords:\n")
                 outputFile.write(str(df))
                 df = pd.DataFrame(columns=["Token", "POS", "XPOS", "Head id", 
-                                           "Dependency", "NER", "NER2"])
+                                           "Dependency", "NER", "NER2", "NER3"])
                 k = 0
                 while k < len(sentence.tokens):
                     token = sentence.tokens[k]
@@ -79,7 +98,9 @@ with open(filename, "r") as input:
                         "Token": token.text, "POS":token.words[0].pos, "XPOS":token.words[0].xpos, 
                         "Head id":token.words[0].head,  
                         "Dependency": token.words[0].deprel, 
-                        "NER": token.ner, "NER2": sentence2.tokens[k].ner}, ignore_index=True)
+                        "NER": token.ner, "NER2": sentence2.tokens[k].ner,
+                        "NER3": sentence3.tokens[k].ner,
+                        "NER4": sentence4.tokens[k].ner}, ignore_index=True)
                     k+=1
                 outputFile.write("\nTokens:\n")
                 outputFile.write(str(df))
