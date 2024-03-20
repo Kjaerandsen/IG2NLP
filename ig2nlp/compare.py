bab65d18186ba132f3545bbce71d9f31a9eef9a4
jonakj <jonakj@stud.ntnu.no>
2024-03-20 09:57:16 +0100

    Updated compare with partial content match detection (cases where one components contents area substring of another component).

--- ig2nlp/compare.py
+++ ig2nlp/compare.py
@@ -36,17 +36,24 @@ def main() -> None:
       # I.e. x(content) == x(content)
       partialPool = []
       #print("1",statement["stanzaParsed"]["components"][14])
+      
+      #True positive scope and symbol, also accounts for invalid nesting
       matches = compareComponentsDirect(statement["manualParsed"]["components"],
                                         statement["stanzaParsed"]["components"],
                                         matches, partialPool)
       #print("2",statement["stanzaParsed"]["components"][14])
+      # True positive scope, wrong symbol
       matches = compareComponentsWrongSymbol(statement["manualParsed"]["components"],
+                                        statement["stanzaParsed"]["components"],
+                                        matches, partialPool)
+      # Partial content matches
+      matches = compareComponentsPartial(statement["manualParsed"]["components"],
                                         statement["stanzaParsed"]["components"],
                                         matches, partialPool)
       
       #print("3",statement["stanzaParsed"]["components"][14])
-      print(matches,"\n")
-      print(partialPool)
+      #print(matches,"\n")
+      #print(partialPool)
 
       statementData = dict()
       for dataPoint in ["name","baseTx","manuTx","autoTx"]:
@@ -66,6 +73,8 @@ def main() -> None:
    print("\n\nPartialPool:\n")
    print(type(outData[0]["partialPool"][0]))
    """
+   print(matches,"\n")
+
 
    with open(outfilename, "w") as output:
       json.dump(outData, output, indent=2)
@@ -96,11 +105,11 @@ def compareComponentsDirect(manual:list, automa:list,
                      output[i][0] += 1
                   else:
                      output[i][1] += 1
-                  # Add the components to the partialPool
-                  entry = {}
-                  entry["ManualComponents"] = [manual[i][j]]
-                  entry["StanzaComponents"] = [automa[i][k]]
-                  partialPool.append(entry)
+                     # Add the components to the partialPool
+                     entry = {}
+                     entry["ManualComponents"] = [manual[i][j]]
+                     entry["StanzaComponents"] = [automa[i][k]]
+                     partialPool.append(entry)
 
                   # Remove the components from the list of components
                   automa[i] = automa[i][:k] + automa[i][k+1:]
@@ -149,6 +158,64 @@ def compareComponentsWrongSymbol(manual:list, automa:list,
                      automaLen -= 1
 
                      print("Match, partial")
+               k+=1
+            j+=1
+
+   return output
+
+def compareComponentsPartial(manual:list, automa:list, 
+                                 output:np.array, partialPool:list[dict]) -> dict:
+   """Directly compares components from manual and automatically annotated statements,
+   removes content matches with incorrect symbols and adds to a count for the partial positive 
+   rate of that specific component
+   """
+
+   for i in range(17):
+      for l in range(17):
+         if manual[i] == None or automa[l] == None:
+            continue
+         j = 0
+         k = 0
+         manualLen = len(manual[i])
+         automaLen = len(automa[l])
+         while j < manualLen:
+            while k < automaLen:
+               if manual[i] != None and automa[i] != None:
+                  #print(type(manual[i]), type(automa[l]))
+                  if automa[l][k]["Content"] in manual[i][j]["Content"]:
+                     print("Match, substring")
+                     print(automa[l][k],manual[i][j])
+                     output[l][1] += 1
+                     print("COMPONENT: ", automa[l][k])
+                     # Add the components to the partialPool
+                     entry = {}
+                     entry["ManualComponents"] = [manual[i][j]]
+                     entry["StanzaComponents"] = [automa[l][k]]
+                     partialPool.append(entry)
+
+                     # Remove the components from the list of components
+                     automa[l] = automa[l][:k] + automa[l][k+1:]
+                     manual[i] = manual[i][:j] + manual[i][j+1:]
+                     manualLen -= 1
+                     automaLen -= 1
+
+                     
+                  elif manual[i][j]["Content"] in automa[l][k]["Content"]:
+                     print(manual[i][j],automa[l][k])
+                     print("Match, substring")
+                     output[l][1] += 1
+                     print("COMPONENT: ", automa[l][k])
+                     # Add the components to the partialPool
+                     entry = {}
+                     entry["ManualComponents"] = [manual[i][j]]
+                     entry["StanzaComponents"] = [automa[l][k]]
+                     partialPool.append(entry)
+
+                     # Remove the components from the list of components
+                     automa[l] = automa[l][:k] + automa[l][k+1:]
+                     manual[i] = manual[i][:j] + manual[i][j+1:]
+                     manualLen -= 1
+                     automaLen -= 1
                k+=1
             j+=1
 
